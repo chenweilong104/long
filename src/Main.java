a7b077592782d37e777e6fbc5c392ff940e6e54d
chenweilong104 <1814390938@qq.com>
2018-08-02 21:16:28 +0800

    word

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!hhhhhhhh");
+
+        Scanner sc = new Scanner(System.in);
+        int k=sc.nextInt();
+        int i,times=0;
+        String str=sc.next();
+        char cur=str.charAt(0);
+        int lena=str.length();
+        for(i=0;i<lena;i++)
+        {
+            if(cur==str.charAt(i))
+            {
+                times++;
+                if(times>=k)
+                {
+                    System.out.println(cur);
+                    System.exit(0);
+                }
+            }
+            else{
+                cur=str.charAt(i);
+                times=1;
+            }
+        }
+        System.out.println("No");
     }
 }
